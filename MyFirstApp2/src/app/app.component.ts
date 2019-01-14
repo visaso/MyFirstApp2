d9e3498e80b91e43eadb6806ccf5979bee20dafc
visaso <visaso@metropolia.fi>
2019-01-14 15:17:00 +0200

    Done

--- MyFirstApp2/src/app/app.component.ts
+++ MyFirstApp2/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +17,8 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    }).catch(err => {
+      console.log('Error: ' + err);
     });
   }
 }
