876b1fed5887e9c682118acd782386f4a38c04c8
visaso <visa.soininen@metropolia.fi>
2019-01-17 14:21:37 +0200

    Task-A done

--- MyFirstApp2/src/app/app.module.ts
+++ MyFirstApp2/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- MyFirstApp2/src/pages/home/home.ts
+++ MyFirstApp2/src/pages/home/home.ts
@@ -1,49 +1,36 @@
 import { Component, Provider } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
-class Pic {
-  constructor(
-    public title: string,
-    public details: string,
-    public thumbnail: string,
-    public original: string,
-  ) {
-  }
-
-}
+import { Pic } from '../../interfaces/pic';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  picArray: Pic[] = [];
+  mediaPath = 'assets/test.json';
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private http: HttpClient) {
   }
 
-  picArray: Pic[] = [
-    {
-      'title': 'Title 1',
-      'details': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-      'thumbnail': 'http://placekitten.com/310/302',
-      'original': 'http://placekitten.com/2048/1920',
-    },
-    {
+  ngOnInit() {
+    this.getImages();
+    console.log('Getting images');
+  }
 
-      'title': 'Title 2',
-      'details': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-      'thumbnail': 'http://placekitten.com/321/300',
-      'original': 'http://placekitten.com/2041/1922',
-    },
-    {
-      'title': 'Title 3',
-      'details': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-      'thumbnail': 'http://placekitten.com/319/301',
-      'original': 'http://placekitten.com/2039/1920',
-    },
-  ];
-
+  getImages() {
+    this.http.get<Pic[]>(this.mediaPath).subscribe(
+      (res: Pic[]) => {
+        this.picArray = res;
+        console.log(this.picArray);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
+
 
 
