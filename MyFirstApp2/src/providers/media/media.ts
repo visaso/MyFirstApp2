2b34404fb173b3cae4e6daf8af3f19fccc3fef62
visaso <visa.soininen@metropolia.fi>
2019-01-21 10:25:05 +0200

    Providers A Completed

---
+++ MyFirstApp2/src/providers/media/media.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pic } from "../../interfaces/pic";
+
+/*
+  Generated class for the MediaProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MediaProvider {
+
+  mediaPath = 'http://media.mw.metropolia.fi/wbma/media';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello MediaProvider Provider');
+  }
+
+  getAllMedia() {
+    return this.http.get<Pic[]>(this.mediaPath);
+  }
+  getSingleMedia(id: any) {
+   return this.http.get<Pic[]>(this.mediaPath + '/' + id);
+  }
+
+}
